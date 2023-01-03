85bf1c6da7374ecfa172abd62c2db12b739838aa
AndrePonce3322 <andreponce417@gmail.com>
2023-01-03 11:18:32 -0600

    sisi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-
-  ngOnInit(): void{
-    let modoOscuro = window.matchMedia("(prefers-color-schema: dark)");
-    if(modoOscuro.matches){
-      console.log("Modo oscuro detectado");
-    }else{
-      console.log("Modo oscuro desactivado");
-    }
-  }
+export class AppComponent {
+  paises=['Honduras', 'USA', 'Canadá'];
 }
