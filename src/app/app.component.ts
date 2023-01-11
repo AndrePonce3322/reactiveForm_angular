94c0e94221de26aadbc83cb47237ab774473ae5e
AndrePonce3322 <andreponce417@gmail.com>
2023-01-11 13:49:36 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,10 @@ import * as mongoose from 'mongoose';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>¡MongoDB works!</h1>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(){
-    mongoose.connect('mongodb://localhost:4545/mydatabase',{ useNewUrlParser: true, useUnifiedTopology: true });
 
-    const db = mongoose.connection;
 
-    db.on('error',console.error.bind(console, 'conection error: '));
-    db.once('open', function(){
-      console.log('Connected to mongoDB');
-    });
-
-  }
 }
--- src/app/contact-reactive/contact-reactive.component.ts
+++ src/app/contact-reactive/contact-reactive.component.ts
@@ -1,17 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-reactive',
   templateUrl: './contact-reactive.component.html',
   styleUrls: ['./contact-reactive.component.css']
 })
-export class ContactReactiveComponent {
-  takeDates(valores: any){
-    alert(`
-    Datos ->
-    Nombre: ${valores.name}
-    Ciudad: ${valores.ciudad}
-    Mayor de edad: ${valores.edad}
-    `);
+export class ContactReactiveComponent implements OnInit{
+
+  formulario!: FormGroup;
+
+  constructor(private readonly fb: FormBuilder){}
+
+  ngOnInit():void{
+    this.formulario = this.initForm();
+    console.clear();
+
   }
+
+  mostrardata:string[] = [];
+  boleano = false;
+
+  tomarDatos(){
+    console.log(this.formulario.value);
+
+    this.mostrardata.push(this.formulario.value);
+    this.boleano = true;
+
+    this.formulario.reset();
+    this.formulario.invalid;
+  }
+
+  initForm(): FormGroup{
+    return this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      checkAdult: [false, [Validators.required]],
+      ciudad: ['', [Validators.required]],
+      textTarea: ['', [Validators.required, Validators.maxLength(300),Validators.minLength(10)]]
+    })
+
+  }
+
+
+
+
 }
