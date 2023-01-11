91d55812b96490549b5dd220209702cf210d3bc3
AndrePonce3322 <andreponce417@gmail.com>
2023-01-11 10:40:25 -0600

    yep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import * as mongoose from 'mongoose';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>¡MongoDB works!</h1>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(){
+    mongoose.connect('mongodb://localhost:4545/mydatabase',{ useNewUrlParser: true, useUnifiedTopology: true });
 
+    const db = mongoose.connection;
+
+    db.on('error',console.error.bind(console, 'conection error: '));
+    db.once('open', function(){
+      console.log('Connected to mongoDB');
+    });
+
+  }
 }
